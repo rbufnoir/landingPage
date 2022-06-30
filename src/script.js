ca5c83307b0096604dcda4d3e8c6a59546a0d8a0
rbufnoir <remi.bufnoir@free.fr>
2022-06-30 15:29:40 +0200

    Merged after conflict resolution

--- src/script.js
+++ src/script.js
@@ -53,42 +53,7 @@ document.addEventListener('wheel', scroll => {
   }
 });
 
-<<<<<<< HEAD
-function removeAnimate(layer) {
-  layer[0].classList.remove('revealer--animate');
-  layer[0].style.bottom = "";
-  layer[0].style.top = "";
-  layer[0].style.animationName = "";
-}
 
-//document.body.getElementsByClassName("revealer")[0].addEventListener("animationend", coucou)
-
-
-function changePageCurrent(child) {
-  console.log(child);
-  const page = document.getElementsByClassName("page--current")[0]
-  if (page.previousElementSibling != null && child === 'top') {
-    page.previousElementSibling.classList.add("page--current");
-    animateSlider(child);
-  }
-  else if (page.nextElementSibling != null && child === 'bottom') {
-    page.nextElementSibling.classList.add("page--current");
-    animateSlider(child);
-  }
-}
-
-function removeCurrentPage (page) {
-  page.classList.remove("page--current");
-}
-
-function animateSlider(direction) {
-  if (direction === "top") {
-    document.getElementsByClassName("revealer")[0].style.bottom = "";
-    document.getElementsByClassName("revealer")[0].style.top = "100%";
-    document.getElementsByClassName("revealer")[0].style.animationName = "animationBot";
-    }
-  else if (direction === "bottom"){
-=======
 function shutterAnimationConfig(direction) {
   if (direction === 'top') {
     document.getElementsByClassName("revealer")[0].style.bottom = "";
@@ -96,25 +61,13 @@ function shutterAnimationConfig(direction) {
     document.getElementsByClassName("revealer")[0].style.animationName = "animationBot";
   }
   else {
->>>>>>> bosseLeJsConnard
     document.getElementsByClassName("revealer")[0].style.bottom = "100%";
     document.getElementsByClassName("revealer")[0].style.top = "";
     document.getElementsByClassName("revealer")[0].style.animationName = "animationTop";
   }
 }
 
-<<<<<<< HEAD
-function animation(direction) {
-  const layer = document.getElementsByClassName("revealer");
-  const page = document.getElementsByClassName("page--current")[0];
 
-  animateSlider(direction);
-  setTimeout(function () {changePageCurrent(direction)}, 1000);
-  setTimeout(function () {removeCurrentPage(page)}, 1000);
-  layer[0].classList.add('revealer--animate');
-  layer[0].addEventListener("animationend", function () {removeAnimate(layer)});
-  layer[0].addEventListener("webkitAnimationEnd", function () {removeAnimate(layer)});
-=======
 function resetShutter(shutter) {
   shutter.style.bottom = "";
   shutter.style.top = "";
@@ -143,7 +96,6 @@ function slideNavigation(direction) {
     shutter.addEventListener("animationend", function () {resetShutter(shutter)});
     setTimeout(function () {nextSlide(direction)}, 1000);
   }
->>>>>>> bosseLeJsConnard
 }
 
 var slideIndex = 1;
